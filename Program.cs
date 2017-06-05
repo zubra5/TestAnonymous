27032ca633a0924128cac281d6a2a9641fbd01c2
zubra5 <larissa.pflyug@gmail.com>
2017-06-05 09:46:19 +0300

    create repo

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+
+namespace TestAnonymous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 5;
+            var b = new[] { (object)1, "3", 5 };
+            var c = new { Name = "Lara", Agr = 33, IsValid = true };
+            var arr = new[] { c };
+            var result = arr.ToList();
+            
+            string jsonString = new JavaScriptSerializer().Serialize(c);
+            string test="test";
+            int cnt = test.GetSymbolCount('t');
+            
+            Console.WriteLine("c.Name={0}", c.Name);
+
+            result.Clear();
+
+            Console.WriteLine(jsonString);
+
+            Console.ReadLine();
+        }
+    }
+}
